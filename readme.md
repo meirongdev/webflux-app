daeaabe6bed635ce52671489820835c9bf2418e9
Meirong <meirongdev@gmail.com>
2024-12-01 08:28:33 +0800

    chore: init project

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Webflux APp
+
+This is a simple webflux demo that uses the following technologies:
+- spring boot 3.4.0
+- spring webflux
+- spring data r2dbc
+- r2dbc postgres
+- spring boot actuator
+- spring boot cache
+- hibernate validator
+- lombok
---
+++ src/main/java/dev/meirong/demos/webflux_app/WebfluxAppApplication.java
@@ -0,0 +1,13 @@
+package dev.meirong.demos.webflux_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxAppApplication.class, args);
+	}
+
+}
